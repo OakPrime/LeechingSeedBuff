4eceb53bde1fdf40cde623e8d5f99b6a46dc9a42
oaksdaniel <dtoaks@hotmail.com>
2022-10-11 00:01:45 -0400

    Initial commit

---
+++ LeechingSeedBuff/LeechingSeedBuff.cs
@@ -0,0 +1,59 @@
+using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using System;
+using R2API;
+using R2API.Utils;
+using System.Collections.Generic;
+
+namespace LeechingSeedBuff
+{
+    //Loads R2API Submodules
+    [R2APISubmoduleDependency(nameof(LanguageAPI))]
+
+    //This is an example plugin that can be put in BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
+    //It's a small plugin that adds a relatively simple item to the game, and gives you that item whenever you press F2.
+
+    //This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    //This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class LeechingSeedBuff : BaseUnityPlugin
+    {
+        //The Plugin GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config).
+        //If we see this PluginGUID as it is on thunderstore, we will deprecate this mod. Change the PluginAuthor and the PluginName !
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "OakPrime";
+        public const string PluginName = "LeechingSeedBuff";
+        public const string PluginVersion = "0.1.0";
+
+        private readonly Dictionary<string, string> DefaultLanguage = new Dictionary<string, string>();
+
+        //The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            try
+            {
+                IL.RoR2.GlobalEventManager.OnHitEnemy += (il) =>
+                {
+                    ILCursor c = new ILCursor(il);
+                    c.TryGotoNext(
+                        x => x.MatchLdloc(out _),
+                        x => x.MatchLdloc(out _),
+                        x => x.MatchConvR4(),
+                        x => x.MatchLdarg(out _),
+                        x => x.MatchLdfld<RoR2.DamageInfo>(nameof(RoR2.DamageInfo.procCoefficient))
+                    );
+                    c.Index += 4;
+                    c.RemoveRange(2);
+                };
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e.Message + " - " + e.StackTrace);
+            }
+        }
+    }
+}
